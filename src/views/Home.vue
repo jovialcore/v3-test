99ccd3b11c436335e9f587d3dd475aa949c3f8f1
Luciano Weslen <luciano.weslen11@gmail.com>
2021-09-06 15:00:09 -0300

    start the project with Vue 3 | Vuex | Router | SCSS | ESLint

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <img alt="JuriDoc" src="../assets/images/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Home extends Vue {}
+</script>
