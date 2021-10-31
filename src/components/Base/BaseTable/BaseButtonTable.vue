0980d7a294cd34daec59aa530e2cf5cddcfae615
Luciano Weslen <luciano@tilit.com.br>
2021-10-31 17:24:34 +0000

    Novos componentes

---
+++ src/components/Base/BaseTable/BaseButtonTable.vue
@@ -0,0 +1,56 @@
+<template>
+  <button class="button-table" :disabled="disabled" :class="classes">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue';
+
+export default defineComponent({
+  props: {
+    selected: { type: Boolean },
+    disabled: { type: Boolean, default: false },
+  },
+  setup(props) {
+    const classes = computed(() => ({
+      selected: props.selected
+    }));
+
+    return {
+      classes,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.button-table {
+  width: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  background: transparent;
+  outline: none !important;
+  padding: 1.5rem;
+  line-height: 0;
+
+  color: $text-dark-grey-5;
+  background: #F7F7F7;
+  border: 1px solid $text-soft-grey;
+  border-radius: 8px;
+
+  transition: background 0.2s ease-in-out;
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+
+  &.selected, &:hover:not(:disabled) {
+    background-color: #5ABE73;
+    color: #F1F6F2;
+  }
+}
+</style>
---
+++ src/components/Base/BaseTag.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="`base-tag ${status}`">
+    {{ t(`BaseTag.status.${status}`) }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+export default defineComponent({
+  props: {
+    status: { type: String, default: "draft"},
+  },
+  setup() {
+    const { t } = useI18n();
+
+    return { t }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.base-tag {
+  width: 100%;
+  padding: 8px;
+  background: $text-dark-grey-2;
+  color: $neutral-2;
+
+  border-radius: 8px;
+  &.approval {
+    background: #5732C2;
+
+  }
+  &.draft {
+    background: #999999;
+  }
+  &.finished {
+    background: #4571AE;
+  }
+}
+</style>
